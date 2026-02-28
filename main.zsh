f5638b3f7c64eacb6b27646ba336339fd59e4cb2
Lepsima <77903853+Lepsima@users.noreply.github.com>
2026-02-28 13:49:05 +0100

    Create main.zsh

---
+++ main.zsh
@@ -0,0 +1,13 @@
+alias celar=clear
+alias caler=clear
+alias claer=clear
+alias cler=clear
+alias cls=clear
+
+
+vibe-check () {
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt autoremove -y
+    sudo apt clean
+}
