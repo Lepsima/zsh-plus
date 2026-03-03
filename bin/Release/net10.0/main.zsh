d54f4c2cd2f4ba6d6f824b1d2014855964597a47
Lepsima <hugo.diaz.morcillo@gmail.com>
2026-03-04 00:03:01 +0100

    n

---
+++ bin/Release/net10.0/main.zsh
@@ -0,0 +1,16 @@
+alias celar=clear
+alias caler=clear
+alias claer=clear
+alias cler=clear
+alias cls=clear
+
+alias neofetch=fastfetch
+
+vibe-check () {
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt autoremove -y
+    sudo apt clean
+}
+
+fastfetch
